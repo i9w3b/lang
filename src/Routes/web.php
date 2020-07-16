681466a40a481018a582d3e3f416c27642594084
Marcelo Sena <suportemarcelosena@gmail.com>
2020-07-16 02:07:17 -0300

    :up: Atualização

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -6,11 +6,13 @@
 |--------------------------------------------------------------------------
 */
 
-Route::get('lang/{locale}', 'LangController@index')->name('locale.index');
+if (config('lang.routes_show') !== false && config('lang.routes_show') === true) {
 
-if (config('lang.routes_show') != false && config('lang.routes_show') == true) {
+    Route::get('lang/{locale}', 'LangController@index')->name('locale.index');
+
     Route::get('manage-language/{lang?}', 'LangController@manageLanguage')->name('manage.language');
     Route::get('create-language', 'LangController@createLanguage')->name('create.language');
     Route::post('store-language-data/{lang}', 'LangController@storeLanguageData')->name('store.language.data');
     Route::post('store-language', 'LangController@storeLanguage')->name('store.language');
+
 }
